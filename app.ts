6c4b216feb21b2b233cbda28c213411e341658a5
Júlia Andrade <julia.andrade@maisunifacisa.com.br>
2025-11-25 12:52:09 -0300

    Realização de conexão com o mongo e integração de login com o front

--- app.ts
+++ app.ts
@@ -5,8 +5,10 @@ import authRouter from "./src/routes/auth/auth.routes";
 const app = express();
 app.use(express.json());
 
-app.use(cors());
-
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(authRouter);
 
 export default app;
